a2823f0a3147d937781bffe3ec90b285a50ba080
Emil Bredenberg <emil.bredenberg@kukkula.local>
2024-02-12 12:45:31 +0200

    Update

--- server/index.js
+++ server/index.js
@@ -23,6 +23,58 @@ app.get('/hae', (req, res) => {
     })
 })
 
+app.delete('/poista', (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM henkilot WHERE id = ?", id, (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    })
+})
+
+
+app.post('/luo', (req, res) => {
+    const nimi = req.body.nimi;
+    const titteli = req.body.titteli;
+    const sahkopostiosoite = req.body.sahkopostiosoite;
+    const puhelinnumero = req.body.puhelinnumero;
+    const id = req.body.id;
+
+    if (id != "") {
+        db.query("UPDATE henkilot set nimi = ?, puhelinnumero = ?, sahkopostiosoite = ?, titteli = ? WHERE id = ?", 
+        [nimi, puhelinnumero, sahkopostiosoite, titteli, id], (err, result) => {
+            if(err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        })
+    } else {
+        db.query("INSERT INTO henkilot (nimi, puhelinnumero, sahkopostiosoite, titteli) values (?,?,?,?)", 
+        [nimi, puhelinnumero, sahkopostiosoite, titteli], (err, result) => {
+            if(err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        })
+    }
+})
+
+app.get('/haehenkilo/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.query("SELECT * FROM henkilot WHERE id = ?", id, (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    })
+})
+
 
 app.listen(3001, () => {
     console.log('Palvelin toimii') 
